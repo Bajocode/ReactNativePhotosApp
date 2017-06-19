9a3316fec4372f4c3128c1f32f70c1f7bbd87fbd
Fabijan Bajo <bajo09@gmail.com>
2017-06-19 08:58:56 +0200

    Execute FLickrSearch request

--- PhotosApp/SearchScreen.js
+++ PhotosApp/SearchScreen.js
@@ -1,28 +1,73 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {
     StyleSheet,
     Text,
     TextInput,
     View,
-    TouchableHighlight,
+    Button,
     ActivityIndicator,
     Image
 } from 'react-native';
+const apiKey = 'a6d819499131071f158fd740860a5a88';
+const baseURLString = 'https://api.flickr.com/services/rest';
+
 
 class SearchScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { searchText: 'Summer' };
+    }
+    _constructURL(searchText) {
+        var params = {
+            api_key: apiKey,
+            method: 'flickr.photos.search',
+            text: searchText,
+            extras: 'url_m',
+            format: 'json',
+            nojsoncallback: '1'
+        };
+        var queryString = Object.keys(params)
+            .map(key => key + '=' + encodeURIComponent(params[key]))
+            .join('&');
+        return baseURLString + '?' + queryString
+    }
+    _executeFetchRequest(url) {
+        fetch(url)
+            .then(response => {
+                response.json();
+                console.log(response);
+            })
+            .catch(error => console.log(error));
+    }
+    _onPressSearch() {
+        var url = this._constructURL(this.state.searchText);
+        this._executeFetchRequest(url);
+    }
     render() {
         return (
-            <TextInput
-                onChangeText={(text) => console.log(text)}
-            />
+            <View style={styles.root}>
+                <View style={styles.searchRow}>
+                    <TextInput style={styles.textInput}
+                        placeholder='Search by keyword'
+                        onChangeText={(searchText) => this.setState({searchText})}
+                    />
+                    <Button
+                        title="Go"
+                        onPress={this._onPressSearch.bind(this)}
+                    />
+                </View>
+            </View>
         );
     }
 }
 
+
 const styles = StyleSheet.create({
-    root: {
-        alignItems: 'center'
-    }
+    root: { marginTop: 64, alignItems: 'center' },
+    searchRow: { flexDirection: 'row' },
+    textInput: { height: 44, flex: 4 },
+    searchButton: { flex: 1 }
 })
+
 
 module.exports = SearchScreen;
